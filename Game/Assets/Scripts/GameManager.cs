8c73ef5782e8344078e8c999248bfae6958526ae
Fernando <fernandooogoncalves@gmail.com>
2025-06-09 17:26:45 -0300

    Menu ESC nao esta pronto
    precisa arrumar

---
+++ Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Nome da Scene do Menu")]
+    public string menuSceneName = "MenuScene";
+
+    void Start()
+    {
+        // Trava o cursor no jogo (para o mouse look funcionar)
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        // Pressione ESC para voltar ao menu
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ReturnToMenu();
+        }
+    }
+
+    public void ReturnToMenu()
+    {
+        Debug.Log("Voltando ao menu...");
+        SceneManager.LoadScene(menuSceneName);
+    }
+
+    // Método para reiniciar o jogo
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
